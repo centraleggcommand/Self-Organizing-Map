0b7ce94c99fad9e7dc1e9b1ec227699ff0e0ca02
centraleggcommand <rlisam@gmail.com>
2010-04-21 19:07:33 -0700

    Integrated use of Lift framework. Upgraded from 2.7.5 to Scala 2.7.7

--- project/build/Som.scala
+++ project/build/Som.scala
@@ -1,6 +1,20 @@
 import sbt._
 
-class SomProject( info: ProjectInfo) extends DefaultProject(info)
+class SomProject( info: ProjectInfo) extends DefaultWebProject(info)
 {
+  val liftVersion = "2.0-M2"
+
+  override def libraryDependencies = Set(
+    "net.liftweb" % "lift-webkit" % liftVersion % "compile->default",
+    "net.liftweb" % "lift-mapper" % liftVersion % "compile->default",
+    "org.mortbay.jetty" % "jetty" % "6.1.22" % "test->default",
+    "com.h2database" % "h2" % "1.2.121",
+    "junit" % "junit" % "4.5" % "test->default",
+    "org.scala-tools.testing" % "specs" % "1.6.1" % "test->default",
+    "org.slf4j" % "slf4j-log4j12" % "1.4.1"
+  ) ++ super.libraryDependencies
+
+  val smackRepo = "m2-repository-smack" at "http://maven.reucon.com/public"
+
   override def compileOptions = super.compileOptions ++ Seq(Unchecked)
 }
--- src/test/scala/somtest1.scala
+++ src/test/scala/somtest1.scala
@@ -1,5 +1,6 @@
 import textprocessing._
 import somservice._
+import scala.collection.mutable.ListBuffer
 
 object SomTest1 {
 
@@ -11,28 +12,16 @@ object SomTest1 {
       try {
         dbagent.createSom
         //setup data
-        /*
-        val mycontent = args.toList.tail
-        println("inserting: " + mycontent)
-        def convToStrList(rem:List[_],res:List[String]):List[String] = {
-          rem match {
-            case Nil => res.reverse
-            case myrem => {
-              val item = myrem.head match { 
-                case i:String => i
-                case unknown => unknown.toString
-              }
-              convToStrList(myrem.tail, item::res)
-            }
+        val inputList = new ListBuffer[String]
+        val input = if( args.length > 1 ) { //List.fromString(args(1),' ')
+          //use regex
+          val noPunc = """\s*\W*([\w\d_-]*)\W*\s*""".r
+          for( noPunc(word) <- noPunc findAllIn args(1) )  inputList += word
+          inputList.toList
           }
-        }
-        val inputList = convToStrList(mycontent,Nil)
-*/
-
-        //val mycontent = "In the spring, the flowers bloom all over the hillside"
-        val inputList = if( args.length > 1 ) List.fromString(args(1),' ')
-                        else Nil
-        val filteredList = StopWords.removeStopwords(inputList)
+          else Nil
+        println("Regex output: " + input)
+        val filteredList = StopWords.removeStopwords(input)
         var stemmer = new Stemmer()
         val stemmedList = filteredList.map(stemmer.stemWord(_))
         //perform insertion
